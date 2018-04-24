d91fc2362dbd7b77abd76044f1c1d6e5701f0a35
Steven Pears <steven.pears@live.co.uk>
2018-04-24 22:14:45 +0100

    Expose Session object through ISkillState for easier use

--- Alexa.NET.StateManagement/ISkillState.cs
+++ Alexa.NET.StateManagement/ISkillState.cs
@@ -1,17 +1,19 @@
 using System.Threading.Tasks;
+using Alexa.NET.Request;
 
 namespace Alexa.NET.StateManagement{
     public interface ISkillState
     {
         Task<T> Get<T>(string key);
         T GetRequest<T>(string key);
-
         T GetSession<T>(string key);
-
         Task<T> GetPersistent<T>(string key);
+
         void SetRequest<T>(string key, T value);
         void SetSession<T>(string key, T value);
 
         Task SetPersistent<T>(string key, T value);
+
+        Session Session { get; }
     }
 }
